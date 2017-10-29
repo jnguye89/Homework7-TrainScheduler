a477d3856d7c95235f6d1c0086f28932e8ddb8ec
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2017-10-29 09:40:17 -0500

    Minutes Away Working

--- assets/main.js
+++ assets/main.js
@@ -11,7 +11,11 @@
 
 var database = firebase.database();
 
+//-----------------------------
+
+
 database.ref().on("value", function(snapshot){
+	//create the header table on each data load
 	var addTable = $("<table>");
 	var addRowHeader = $("<thead>");
 	var addRow = $("<tr>");
@@ -20,7 +24,6 @@ database.ref().on("value", function(snapshot){
 
 
 	addTable.addClass("table table-striped table-hover");
-
 	addRow.append("<th>Train Name</th>");
 	addRow.append("<th>Destination</th>");
 	addRow.append("<th>Frequency (min)</th>");
@@ -32,23 +35,49 @@ database.ref().on("value", function(snapshot){
 	$("#train-table").html(addTable);
 	
 	snapshot.forEach(function(childSnapshot){
+
+		var currentTime = miliNowFunction();
+		var timeSplit = childSnapshot.val().firstTime.split(":");
+		
+		var firstTrainMilli = (timeSplit[0] * 60 * 60 * 1000) + (timeSplit[1] * 60 * 1000);
+
+		while (currentTime > firstTrainMilli) {
+			firstTrainMilli  = firstTrainMilli + (childSnapshot.val().trainInterval * 60 * 1000);
+		}
+
+		var lastTrain = firstTrainMilli - (childSnapshot.val().trainInterval * 60 * 1000);
+
+		var nextTrainMilli = currentTime - lastTrain;
+		console.log(nextTrainMilli);
+		var nextTrainMin = nextTrainMilli/1000/60;
+		console.log(Math.floor(nextTrainMin));
+
+
+		//display each train schedule for each object in database
 		var addRow = $("<tr>");
 		var addRowBody = $("<tbody>");
-		
 		addRow.append("<td>" + childSnapshot.val().trainName);
 		addRow.append("<td>" + childSnapshot.val().destination);
 		addRow.append("<td>" + childSnapshot.val().trainInterval);
 		addRow.append("<td>" + childSnapshot.val().trainInterval);
-		addRow.append("<td>" + childSnapshot.val().trainInterval);
+		addRow.append("<td>" + Math.floor(nextTrainMin));
 		addRowBody.append(addRow);
 		$(".table").append(addRowBody);
+
+		
+		
+
+		// console.log(firstTrainMilli);
 
 	})
 })
 
 $("button").on("click", function() {
+	//prevent default submit
   event.preventDefault();
 
+
+  	//get input information when submit button is clicked
   var trainName = $("#train-name").val().trim();
   var destination = $("#train-destination").val().trim();
   var firstTime = $("#train-time").val().trim();
@@ -59,6 +88,8 @@ $("button").on("click", function() {
   console.log("First Train Time: " + firstTime);
   console.log("Train Frequency: " + trainInterval);
 
+
+  //push data to firebase
   database.ref().push({
   	trainName: trainName,
   	destination: destination,
@@ -66,6 +97,22 @@ $("button").on("click", function() {
   	trainInterval: trainInterval,
 
   })
-
-
 })
+
+var miliNowFunction = function(){
+
+	//convert the current time to milliseconds
+		var dateNowHour = moment().hour();
+		var dateNowMinutes = moment().minute();
+		var dateNowSeconds = moment().second();
+		var dateNowMilli = moment().millisecond();
+		var dateNow = (dateNowHour*60*60*1000)+(dateNowMinutes*60*1000)+(dateNowSeconds*1000)+dateNowMilli;
+	
+		
+		return dateNow;
+}
+
+var firstTrainMilliFunction = function(){
+
+	
+}
